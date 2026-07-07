c07366f4835d1f19c200cfd8905b8c48cc8a4007
Vakas Ariz <vakasariz10@gmail.com>
2026-07-07 23:51:44 +0530

    password reset

--- schemas.py
+++ schemas.py
@@ -52,3 +52,14 @@ class PaginatedPostsResponse(BaseModel):
     skip: int
     limit: int
     has_more: bool
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr = Field(max_length=120)
+
+class RequestPasswordReset(BaseModel):
+    token: str
+    new_password: str = Field(min_length=8)
+
+class ChangePasswordRequest(BaseModel):
+    current_password: str
+    new_password: str = Field(min_length=8)
